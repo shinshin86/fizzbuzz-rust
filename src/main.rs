8ce0b05d62c126a9c7b303864e5c2a11a3187ab9
shinshin86 <beagles1986@gmail.com>
2021-10-16 22:12:02 +0900

    Add test

--- src/main.rs
+++ src/main.rs
@@ -15,3 +15,22 @@ fn main() {
         println!("{}", fizzbuzz(n));
     }
 }
+
+#[test]
+fn it_works() {
+    assert_eq!(fizzbuzz(1), "1");
+    assert_eq!(fizzbuzz(2), "2");
+    assert_eq!(fizzbuzz(3), "Fizz");
+    assert_eq!(fizzbuzz(4), "4");
+    assert_eq!(fizzbuzz(5), "Buzz");
+    assert_eq!(fizzbuzz(6), "Fizz");
+    assert_eq!(fizzbuzz(7), "7");
+    assert_eq!(fizzbuzz(8), "8");
+    assert_eq!(fizzbuzz(9), "Fizz");
+    assert_eq!(fizzbuzz(10), "Buzz");
+    assert_eq!(fizzbuzz(11), "11");
+    assert_eq!(fizzbuzz(12), "Fizz");
+    assert_eq!(fizzbuzz(13), "13");
+    assert_eq!(fizzbuzz(14), "14");
+    assert_eq!(fizzbuzz(15), "FizzBuzz");
+}
